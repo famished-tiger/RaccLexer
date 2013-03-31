5478b144c737b485e3e1655ba799eb4c0699b98a
famished.tiger <famished.tiger@yahoo.com>
2013-03-31 21:14:04 +0200

    Added requires in racc-lexer.rb file
    The file racc-lexer.rb now includes the dependencies to key classes of
    RaccLexer.

--- lib/racc-lexer.rb
+++ lib/racc-lexer.rb
@@ -1,6 +1,8 @@
 # File: racc-lexer.rb
 # This file acts as a jumping-off point for loading dependencies expected for a racc-lexer gem user.
 
-require_relative ""
+require_relative "racc-lexer/abstract-lexer"
+require_relative "racc-lexer/non-token-rule"
+require_relative "racc-lexer/lexer-rule-dsl" 
 
 # End of file
